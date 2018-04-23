0df30d4eab24ddc673b5198e8e9d3d795489cf1c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxGridView;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) { }
+    protected void detailGrid_CustomCallback(object sender, ASPxGridViewCustomCallbackEventArgs e) {
+        Session["CategoryID"] = e.Parameters;
+        detailGrid.DataBind();
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# ASPxGridView - Show a separate detail grid when changing a master grid’s focused row using keyboard
+
+
+<p>This example demonstrates how to implement a master-detail scenario with two separate grids. The KeyboardSupport property allows changing the master grid's focused row by pressing navigation keys. The detail grid updates information when the master grid's focused row is changed.</p><br />
+
+
+<br/>
+
+
